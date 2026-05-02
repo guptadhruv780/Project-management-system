c3887a5f646bbace1c511f104cfba3a00741fc5e
DHRUV GUPTA <guptadhruv780@gmail.com>
2026-05-02 21:36:05 +0530

    Ulitmate fix: removed root package.json and simplified prisma

--- teamflow/backend/src/lib/prisma.js
+++ teamflow/backend/src/lib/prisma.js
@@ -1,3 +1,3 @@
-const { PrismaClient } = require('../../prisma/generated-client');
+const { PrismaClient } = require('../../generated-client');
 const prisma = new PrismaClient();
 module.exports = prisma;
